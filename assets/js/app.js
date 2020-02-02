621affa8e2c66fb19838e5ac101425e7d4b62925
Gabe <hgabka@dev.hgabka.eu>
2020-02-02 15:50:49 +0100

    webpack refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
 require('../css/media-chooser.css');
+
+import bulkUpload from './bulkupload.js';
+var dndUpload = require('./dndupload.js').dndUpload;
+var hgabkamedia = {};
+
+hgabkamedia.app = (function($, window, undefined) {
+    var init;
+
+    // General App init
+    init = function () {
+        bulkUpload.bulkUpload.init();
+        dndUpload.dndUpload.init();
+    }
+    return {
+        init: init
+    };
+
+})(jQuery, window);
+
+$(function() {
+    hgabkamedia.app.init();
+});
