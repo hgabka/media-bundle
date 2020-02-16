67bbfb7e026a0ab4abf8c9bc06fe5f20e328aace
hgabka <hgabka@gmail.com>
2020-02-16 21:28:20 +0100

    Update MediaCreatorService.php

--- src/Helper/Services/MediaCreatorService.php
+++ src/Helper/Services/MediaCreatorService.php
@@ -1,92 +1,81 @@
 <?php
 
-namespace App\Command;
+namespace Hgabka\MediaBundle\Helper\Services;
 
-use App\Entity\Product;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
 use Hgabka\MediaBundle\Entity\Folder;
-use Hgabka\MediaBundle\Helper\Services\MediaCreatorService;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Finder\Finder;
+use Hgabka\MediaBundle\Entity\Media;
+use Hgabka\MediaBundle\Repository\FolderRepository;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\File\File;
 
-class CopyProductImagesCommand extends Command
+/**
+ * Service to easily add a media file to an existing media folder.
+ * This is especially useful in migrations or places where you want to automate the uploading of media.
+ *
+ * Class MediaCreatorService
+ */
+class MediaCreatorService
 {
-    protected static $defaultName = 'haldepo:products:images';
-
-    /** @var EntityManagerInterface */
-    protected $doctrine;
-
-    /** @var MediaCreatorService */
-    protected $creator;
-
-    /** @var string */
-    protected $projectDir;
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
 
     /**
-     * CopyProductImagesCommand constructor.
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var FolderRepository
+     */
+    protected $folderRepository;
+
+    /**
+     * Constructor.
      *
-     * @param EntityManagerInterface $doctrine
-     * @param                        $projectDir
+     * @param ContainerInterface $container
      */
-    public function __construct(EntityManagerInterface $doctrine, MediaCreatorService $creator, $projectDir)
+    public function __construct(ContainerInterface $container)
     {
-        parent::__construct();
-
-        $this->doctrine = $doctrine;
-        $this->creator = $creator;
-        $this->projectDir = $projectDir;
+        $this->container = $container;
+        $this->em = $container->get('doctrine')->getManager();
+        $this->folderRepository = $this->em->getRepository('HgabkaMediaBundle:Folder');
     }
 
     /**
-     * {@inheritdoc}
+     * @param $filePath string  The full filepath of the asset you want to upload. The filetype will be automatically detected.
+     * @param $folderId integer For now you still have to manually pass the correct folder ID
+     *
+     * @return Media
      */
-    protected function configure()
+    public function createFile($filePath, $folderId, $andFlush = true)
     {
-        $this
-            ->setName(static::$defaultName)
-            ->setDescription('Copy product images')
-            ->setHelp(
-                <<<'EOT'
-The <info>haldepo:products:images</info> command copies product images from old system:
-EOT
-            )
-        ;
-    }
+        $fileHandler = $this->container->get('hgabka_media.media_handlers.file');
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
-        set_time_limit(0);
-        ini_set('memory_limit', '-1');
+        // Get file from FilePath.
+        $data = new File($filePath, true);
 
-        $folder = $this->doctrine->getRepository(Folder::class)->findOneByInternalName('product');
-
-        $finder = new Finder();
-        $finder->files()->in($this->projectDir.'/var/product');
-        $batchSize = 100;
-
-        foreach ($finder as $result) {
-            $filePath = $result->getRealPath();
-            $productId = $result->getBasename('.'.$result->getExtension());
-
-            /** @var Product $product */
-            $product = $this->doctrine->getRepository(Product::class)->findOneByProductId($productId);
-            if (!$product || !empty($product->getPicture())) {
-                continue;
-            }
-
-            $media = $this->creator->createFile($filePath, $folder->getId(), false);
-            $product->setPicture($media);
-
-            if (--$batchSize <= 0) {
-                $this->doctrine->flush();
-                $this->doctrine->clear();
-                $batchSize = 100;
-            }
+        /** @var $media Media */
+        $media = $fileHandler->createNew($data);
+        if ($folderId instanceof Folder) {
+            $folder = $folderId;
+        } else {
+            /** @var $folder Folder */
+            $folder = $this->folderRepository->getFolder($folderId);
         }
+        $media->setFolder($folder);
+
+        $fileHandler->prepareMedia($media);
+        $fileHandler->updateMedia($media);
+        $fileHandler->saveMedia($media);
+
+        $this->em->persist($media);
+        if ($andFlush) {
+            $this->em->flush();
+        }
+
+        return $media;
     }
 }
