fc758d8a0fbbe13b2063413e83c3b634c0166dc4
Gabe <hgabka@harmatgabor.eu>
2022-01-14 19:59:27 +0100

    media multi type

---
+++ src/Form/Type/FileAndIdToMediaTransformer.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace Hgabka\MediaBundle\Form\Type;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Hgabka\MediaBundle\Entity\Folder;
+use Hgabka\MediaBundle\Entity\Media;
+use Hgabka\MediaBundle\Helper\MediaManager;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+
+class FileAndIdToMediaTransformer implements DataTransformerInterface
+{
+    /** @var null|string */
+    protected $mediaName;
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /** @var Folder */
+    private $folder;
+
+    /**
+     * @var CurrentValueContainer
+     */
+    private $currentValueContainer;
+
+    /** @var MediaManager */
+    private $mediaManager;
+
+    /**
+     * @param ObjectManager         $objectManager         The object manager
+     * @param CurrentValueContainer $currentValueContainer The current value container
+     * @param null|mixed            $mediaName
+     */
+    public function __construct(EntityManagerInterface $objectManager, CurrentValueContainer $currentValueContainer, MediaManager $manager, Folder $folder = null, $mediaName = null)
+    {
+        $this->objectManager = $objectManager;
+        $this->folder = $folder;
+        $this->currentValueContainer = $currentValueContainer;
+        $this->mediaManager = $manager;
+        $this->mediaName = $mediaName;
+    }
+
+    /**
+     * @param Media $entity The value in the original representation
+     *
+     * @throws UnexpectedTypeException   when the argument is not an object
+     * @throws \InvalidArgumentException when the parameter is a collection
+     *
+     * @return mixed The value in the transformed representation
+     */
+    public function transform($entity)
+    {
+        if (empty($entity)) {
+            return [
+                'id' => '',
+            ];
+        }
+        if (!\is_object($entity)) {
+            throw new UnexpectedTypeException($entity, 'object');
+        }
+        if ($entity instanceof Collection) {
+            throw new \InvalidArgumentException('Expected an object, but got a collection. Did you forget to pass "multiple=true" to an entity field?');
+        }
+        $this->currentValueContainer->setCurrentValue($entity);
+
+        return [
+            'ent' => $entity,
+            'id' => $entity->getId(),
+        ];
+    }
+
+    /**
+     * @param string $key
+     * @param mixed  $value
+     *
+     * @throws UnexpectedTypeException       when the parameter is not numeric
+     * @throws TransformationFailedException when the media item cannot be loaded/found
+     *
+     * @return Media
+     */
+    public function reverseTransform($value)
+    {
+        dump($value);
+        if (!empty($value) && !empty($value['file'])) {
+            if ($value['file']->isValid()) {
+                $entity = $this->mediaManager->createNew($value['file']);
+                $this->objectManager->persist($entity);
+                if (!empty($this->mediaName)) {
+                    $entity->setName($this->mediaName);
+                }
+                $entity->setFolder($this->folder);
+                $this->currentValueContainer->setCurrentValue($entity);
+
+                return $entity;
+            }
+
+            return $value['ent'] ?? null;
+        }
+
+        if (empty($value['id'])) {
+            return null;
+        }
+
+        if (!empty($value['ent']) && $value['ent']->getId() === (int) $value['id']) {
+            $this->currentValueContainer->setCurrentValue($value['ent']);
+
+            return $value['ent'];
+        }
+
+        if (!($entity = $this->objectManager->getRepository(Media::class)->find($value['id']))) {
+            throw new TransformationFailedException(sprintf('The entity with key "%s" could not be found', $key));
+        }
+
+        $this->currentValueContainer->setCurrentValue($entity);
+
+        return $entity;
+    }
+}
---
+++ src/Form/Type/MediaMultiType.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace Hgabka\MediaBundle\Form\Type;
+
+namespace Hgabka\MediaBundle\Form\Type;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Hgabka\MediaBundle\Entity\Folder;
+use Hgabka\MediaBundle\Helper\MediaManager;
+use Hgabka\UtilsBundle\Helper\HgabkaUtils;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaMultiType extends AbstractType
+{
+    /**
+     * @var MediaManager
+     */
+    protected $mediaManager;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $objectManager;
+
+    /** @var HgabkaUtils */
+    protected $hgabkaUtils;
+
+    /**
+     * @param MediaManager  $mediaManager  The media manager
+     * @param ObjectManager $objectManager The media manager
+     * @param mixed         $utils
+     */
+    public function __construct($mediaManager, $objectManager, $utils)
+    {
+        $this->mediaManager = $mediaManager;
+        $this->objectManager = $objectManager;
+        $this->hgabkaUtils = $utils;
+    }
+
+    /**
+     * Builds the form.
+     *
+     * This method is called for each type in the hierarchy starting form the
+     * top most type. Type extensions can further modify the form.
+     *
+     * @param FormBuilderInterface $builder The form builder
+     * @param array                $options The options
+     *
+     * @see FormTypeExtensionInterface::buildForm()
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $folder = null;
+        $repo = $this->objectManager->getRepository(Folder::class);
+        if (!empty($options['folderid'])) {
+            $folder = $repo->getFolder($options['folderid']);
+        }
+        if (!$folder && !empty($options['foldername'])) {
+            $folder = $repo->findOneByInternalName($options['foldername']);
+        }
+        if (!$folder && !empty($options['folder']) && $options['folder'] instanceof Folder) {
+            $folder = $options['folder'];
+        }
+        if (!$folder) {
+            if (!empty($options['foldername']) && !empty($options['parentfolder'])) {
+                $parentFolder = $options['parentfolder'] instanceof Folder
+                    ? $parentFolder
+                    : $repo->findOneByInternalName($options['parentfolder']);
+                if (!$parentFolder) {
+                    $parentFolder = $repo->getFirstTopFolder();
+                }
+                $folder = new Folder();
+                $folder
+                    ->setParent($parentFolder)
+                    ->setInternalName($options['foldername'])
+                    ->setCurrentLocale($this->hgabkaUtils->getCurrentLocale())
+                    ->setName($options['foldername'])
+                ;
+                $this->objectManager->persist($folder);
+                $this->objectManager->flush();
+            } else {
+                $folder = $repo->getFirstTopFolder();
+            }
+        }
+        $builder->add('id', HiddenType::class);
+        $builder->add('file', FileType::class);
+
+        $builder->addViewTransformer(
+            new FileAndIdToMediaTransformer($this->objectManager, $options['current_value_container'], $this->mediaManager, $folder, $options['medianame']),
+            true
+        );
+
+        $builder->setAttribute('chooser', $options['chooser']);
+        $builder->setAttribute('mediatype', $options['mediatype']);
+        $builder->setAttribute('foldername', $options['foldername']);
+        $builder->setAttribute('folderid', $options['folderid']);
+        $builder->setAttribute('editor_filter', $options['editor_filter']);
+        $builder->setAttribute('editor_filter_retina', $options['editor_filter_retina']);
+    }
+
+    public function getParent()
+    {
+        return FormType::class;
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'media_multi';
+    }
+
+    /**
+     * Sets the default options for this type.
+     *
+     * @param OptionsResolver $resolver the resolver for the options
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'compound' => true,
+                'chooser' => 'HgabkaMediaBundle_chooser',
+                'current_value_container' => new CurrentValueContainer(),
+                'foldername' => null,
+                'medianame' => null,
+                'mediatype' => null,
+                'folderid' => null,
+                'folder' => null,
+                'parentfolder' => null,
+                'error_bubbling' => false,
+                'editor_filter' => 'media_list_thumbnail',
+                'editor_filter_retina' => 'media_list_thumbnail_retina',
+            ]
+        );
+    }
+
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['chooser'] = $form->getConfig()->getAttribute('chooser');
+        $view->vars['mediatype'] = $form->getConfig()->getAttribute('mediatype');
+        $view->vars['mediamanager'] = $this->mediaManager;
+        $view->vars['foldername'] = $form->getConfig()->getAttribute('foldername');
+        $view->vars['folderid'] = $form->getConfig()->getAttribute('folderid');
+        $view->vars['editor_filter'] = $form->getConfig()->getAttribute('editor_filter');
+        $view->vars['editor_filter_retina'] = $form->getConfig()->getAttribute('editor_filter_retina');
+    }
+}
