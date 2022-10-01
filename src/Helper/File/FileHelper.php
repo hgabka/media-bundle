fee4f9259414ea0f90c8ff4488c6fc222b0c0eeb
hgabka <hgabka@gmail.com>
2022-10-01 10:16:39 +0200

    Update FileHelper.php

--- src/Helper/File/FileHelper.php
+++ src/Helper/File/FileHelper.php
@@ -28,9 +28,15 @@ class FileHelper
      */
     protected $path;
 
-    public function __construct(Media $media)
+    /**
+     * @var string
+     */
+    protected $mediaPath;    
+
+    public function __construct(Media $media, string $mediaPath)
     {
         $this->media = $media;
+        $this->ediaPath = $mediaPath;
     }
 
     /**
@@ -145,7 +151,7 @@ class FileHelper
             $this->media->setContent($file);
             $this->media->setContentType($file->getMimeType());
             $this->media->setUrl(
-                '/uploads/media/' . $this->media->getUuid() . '.' . $this->media->getContent()->getExtension()
+                $this->mediaPath . $this->media->getUuid() . '.' . $this->media->getContent()->getExtension()
             );
         }
     }
