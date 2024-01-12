faf136c7a794a21c03fce5eaad64570c1123d2d7
hgabka <hgabka@gmail.com>
2024-01-12 14:36:46 +0100

    Update FileToMediaTransformer.php

--- src/Form/Type/FileToMediaTransformer.php
+++ src/Form/Type/FileToMediaTransformer.php
@@ -57,7 +57,7 @@ class FileToMediaTransformer implements DataTransformerInterface
      *
      * @return mixed The value in the transformed representation
      */
-    public function transform($entity)
+    public function transform(mixed $entity): mixed
     {
         if (empty($entity)) {
             return [
@@ -87,7 +87,7 @@ class FileToMediaTransformer implements DataTransformerInterface
      *
      * @return Media
      */
-    public function reverseTransform($value)
+    public function reverseTransform(mixed $value): mixed
     {
         if (!empty($value) && !empty($value['file'])) {
             if ($value['file']->isValid()) {
