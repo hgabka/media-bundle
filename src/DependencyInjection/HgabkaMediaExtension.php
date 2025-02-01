a186568777619951ce0a2eb27aa37333642997a6
hgabka <hgabka@gmail.com>
2025-02-01 11:55:18 +0100

    Update HgabkaMediaExtension.php

--- src/DependencyInjection/HgabkaMediaExtension.php
+++ src/DependencyInjection/HgabkaMediaExtension.php
@@ -5,8 +5,8 @@ namespace Hgabka\MediaBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Yaml\Yaml;
 
 /**
