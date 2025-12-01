ea716dac2a91276127a6a20a65ed8ae11a0b6f92
hgabka <hgabka@gmail.com>
2025-12-01 12:20:07 +0100

    Update Configuration.php

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -39,6 +39,15 @@ class Configuration implements ConfigurationInterface
                     ->prototype('scalar')->end()
                 ->end()
                 ->integerNode('folder_depth')->defaultValue(4)->end()
+                ->scalarNode('protected_media_download_role')->defaultValue('ROLE_MEDIA_ADMIN')->end()
+                ->arrayNode('default_ckeditor_folders')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->scalarNode('images')->defaultValue('imageroot')->end()
+                        ->scalarNode('files')->defaultValue('fileroot')->end()
+                    ->end()
+                ->end()
+
             ->end();
 
         return $treeBuilder;
